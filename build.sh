dfc50d09428de7138bb0be11db5455a334ac8cdb
Peijun Ma <peijun.ma@protonmail.com>
2019-02-05 15:39:16 -0500

    Statically link the OpenTTD binary

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 set -e
-pkgname=openttd-jgrpp-git
-_installname=openttd
 
 build() {
     ./configure \
-        --prefix-dir=$HOME/OpenTTD \
-        --binary-name=$_installname \
-        --binary-dir=bin \
-        --data-dir=share/$_installname \
-        --install-dir=$HOME/OpenTTD \
-        --doc-dir=share/doc/$_installname \
         --menu-name="OpenTTD" \
-        --personal-dir=.$_installname  \
-        --without-liblzo2
+        --without-liblzo2 \
+        --enable-static
 
     make -j
 }
